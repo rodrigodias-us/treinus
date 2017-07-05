fd0093fa642cac48aaa8c525ab7c29e0762a816d
Rodrigo Dias <rodrigogmdias@gmail.com>
2017-07-05 13:22:29 -0300

    Initial Commit

---
+++ Treinus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Treinus
+//
+//  Created by Rodrigo Dias on 05/07/17.
+//  Copyright © 2017 Rodrigo Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
